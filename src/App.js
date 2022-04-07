d2dbd7fe3ff2d7e2f081105b5e5195b54aa2772b
Vimal Thanikachalam <vimaleurakaa@gmail.com>
2022-04-07 09:03:39 +0530

    animation handler updated

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ const App = () => {
   const [color, setColor] = useState(false);
 
   const randomAnimation = [
-    'animate__fadeInDownBig',
-    'animate__bounceIn', 
-    'animate__bounceInDown', 
-    'animate__bounceInLeft',
-    'animate__bounceInRight',
-    'animate__bounceInUp',
-    'animate__fadeIn',
-    'animate__fadeInBottomLeft',
-    'animate__fadeInTopLeft',
-    'animate__lightSpeedInRight',
-    'animate__rotateIn',
-    'animate__zoomIn',
-    'animate__zoomInLeft'
-    ]
+    "animate__fadeInDownBig",
+    "animate__bounceIn",
+    "animate__bounceInDown",
+    "animate__bounceInLeft",
+    "animate__bounceInRight",
+    "animate__bounceInUp",
+    "animate__fadeIn",
+    "animate__fadeInBottomLeft",
+    "animate__fadeInTopLeft",
+    "animate__lightSpeedInRight",
+    "animate__rotateIn",
+    "animate__zoomIn",
+    "animate__zoomInLeft"
+  ];
 
-  const [animation, setAnimation] = useState('animate__bounceIn');
+  const [animation, setAnimation] = useState("animate__bounceIn");
 
   const ref = useRef({
     interval: 500,
-    constrain: 50,
-    lastValue: numbers.at(-1),
+    constrain: 30,
+    lastValue: numbers.at(-1)
   });
 
   const { interval, constrain, lastValue } = ref.current;
@@ -50,10 +50,8 @@ const App = () => {
     }
   };
 
-  console.log(color);
-
   const colorHandler = (value) => {
-  if (value % 6 === 0) {
+    if (value % 6 === 0) {
       return "orange";
     } else if (value % 3 === 0) {
       return "green";
@@ -63,32 +61,28 @@ const App = () => {
   };
 
   const setRadomAnimation = () => {
-   const _anim = randomAnimation[Math.floor(Math.random() * randomAnimation.length)];
-   setAnimation(_anim);
-  }
-
-
-  const animationStateHandler = (index) => {
-    if (index + 1 === constrain) {
-      setColor(true);
-    }
+    const _anim =
+      randomAnimation[Math.floor(Math.random() * randomAnimation.length)];
+    setAnimation(_anim);
   };
 
   return (
     <>
       <div className="container">
-      <div className="random_container">
-      <button className="random" onClick={setRadomAnimation}>
-        Random Animation
-      </button>
-      </div>
+        <div className="random_container">
+          <button className="random" onClick={setRadomAnimation}>
+            Random Animation
+          </button>
+        </div>
         <ul className="list__group">
           {numbers?.map((it, i) => {
             return (
               <li
                 className={`animate__animated list__item ${animation}`}
                 style={{ animationDelay: i * 200 + "ms" }}
-                onAnimationEnd={() => animationStateHandler(i)}
+                {...(i + 1 === constrain && {
+                  onAnimationEnd: () => setColor(true)
+                })}
                 key={it}
               >
                 <span {...(color && { className: colorHandler(it) })}>
